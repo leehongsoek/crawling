daa06c3545f251884f6cacd180e1f88b3ff3f1ee
이홍석 <lhs0806@nate.com>
2017-05-30 19:52:28 +0900

    테이블 레이아웃

--- mtns_kofic.sql
+++ mtns_kofic.sql
@@ -3,12 +3,106 @@
 -- http://www.phpmyadmin.net
 --
 -- 호스트: localhost
--- 처리한 시간: 17-05-17 19:07
+-- 처리한 시간: 17-05-30 18:34
 -- 서버 버전: 5.0.26
 -- PHP 버전: 5.1.6
 --
 -- 데이터베이스: `mtns_kofic`
 --
+
+-- --------------------------------------------------------
+
+--
+-- 테이블 구조 `cgv_movies`
+--
+
+CREATE TABLE `cgv_movies` (
+  `moviecode` varchar(5) NOT NULL,
+  `moviename` varchar(100) NOT NULL,
+  `releasedate` varchar(8) NOT NULL,
+  PRIMARY KEY  (`moviecode`)
+) ENGINE=MyISAM DEFAULT CHARSET=euckr;
+
+-- --------------------------------------------------------
+
+--
+-- 테이블 구조 `cgv_regions`
+--
+
+CREATE TABLE `cgv_regions` (
+  `regioncode` varchar(3) NOT NULL,
+  `regionname` varchar(10) NOT NULL,
+  PRIMARY KEY  (`regioncode`)
+) ENGINE=MyISAM DEFAULT CHARSET=euckr;
+
+-- --------------------------------------------------------
+
+--
+-- 테이블 구조 `cgv_theater`
+--
+
+CREATE TABLE `cgv_theater` (
+  `theatercode` varchar(4) NOT NULL,
+  `regioncode` varchar(3) NOT NULL,
+  `regionname` varchar(10) NOT NULL,
+  `theatername` varchar(100) NOT NULL,
+  PRIMARY KEY  (`theatercode`)
+) ENGINE=MyISAM DEFAULT CHARSET=euckr;
+
+-- --------------------------------------------------------
+
+--
+-- 테이블 구조 `cgv_tickecting1`
+--
+
+CREATE TABLE `cgv_tickecting1` (
+  `playdate` varchar(8) NOT NULL,
+  `theatercode` varchar(4) NOT NULL,
+  `moviecode` varchar(5) NOT NULL,
+  `theatername` varchar(100) NOT NULL,
+  `moviename` varchar(100) NOT NULL,
+  PRIMARY KEY  (`playdate`,`theatercode`,`moviecode`)
+) ENGINE=MyISAM DEFAULT CHARSET=euckr;
+
+-- --------------------------------------------------------
+
+--
+-- 테이블 구조 `cgv_tickecting2`
+--
+
+CREATE TABLE `cgv_tickecting2` (
+  `playdate` varchar(8) NOT NULL,
+  `theatercode` varchar(4) NOT NULL,
+  `moviecode` varchar(5) NOT NULL,
+  `gunbunno` int(11) NOT NULL,
+  `theatername` varchar(100) NOT NULL,
+  `moviename` varchar(100) NOT NULL,
+  `filmtype` varchar(50) NOT NULL,
+  `roomfloor` varchar(50) NOT NULL,
+  `totalseat` varchar(10) NOT NULL,
+  PRIMARY KEY  (`playdate`,`theatercode`,`moviecode`,`gunbunno`)
+) ENGINE=MyISAM DEFAULT CHARSET=euckr;
+
+-- --------------------------------------------------------
+
+--
+-- 테이블 구조 `cgv_tickecting3`
+--
+
+CREATE TABLE `cgv_tickecting3` (
+  `playdate` varchar(8) NOT NULL,
+  `theatercode` varchar(4) NOT NULL,
+  `moviecode` varchar(5) NOT NULL,
+  `gunbunno` int(11) NOT NULL,
+  `timeno` int(11) NOT NULL,
+  `theatername` varchar(100) NOT NULL,
+  `moviename` varchar(100) NOT NULL,
+  `filmtype` varchar(50) NOT NULL,
+  `playtime` varchar(50) NOT NULL,
+  `playinfo` varchar(10) NOT NULL,
+  `playetc` varchar(10) NOT NULL,
+  PRIMARY KEY  (`playdate`,`theatercode`,`moviecode`,`gunbunno`,`timeno`)
+) ENGINE=MyISAM DEFAULT CHARSET=euckr;
 
 -- --------------------------------------------------------
 
@@ -167,6 +261,60 @@ CREATE TABLE `kofic_theather` (
 -- --------------------------------------------------------
 
 --
+-- 테이블 구조 `lotte_cinemas`
+--
+
+CREATE TABLE `lotte_cinemas` (
+  `cinemaid` varchar(4) NOT NULL,
+  `special_yn` varchar(1) NOT NULL,
+  `sortsequence` int(11) NOT NULL,
+  `cinemaname` varchar(100) NOT NULL,
+  PRIMARY KEY  (`cinemaid`)
+) ENGINE=MyISAM DEFAULT CHARSET=euckr;
+
+-- --------------------------------------------------------
+
+--
+-- 테이블 구조 `lotte_moviedata`
+--
+
+CREATE TABLE `lotte_moviedata` (
+  `moviecode` varchar(5) NOT NULL,
+  `moviename` varchar(200) NOT NULL,
+  `moviegenrename` varchar(50) NOT NULL,
+  `bookingyn` varchar(1) NOT NULL,
+  `releasedate` varchar(8) NOT NULL,
+  `viewgradename` varchar(100) NOT NULL,
+  PRIMARY KEY  (`moviecode`)
+) ENGINE=MyISAM DEFAULT CHARSET=euckr;
+
+-- --------------------------------------------------------
+
+--
+-- 테이블 구조 `lotte_ticketingdata`
+--
+
+CREATE TABLE `lotte_ticketingdata` (
+  `cinemaid` varchar(4) NOT NULL,
+  `roomid` varchar(2) NOT NULL,
+  `moviecode` varchar(5) NOT NULL,
+  `cinemaname` varchar(100) NOT NULL,
+  `groupname` varchar(20) NOT NULL,
+  `screenname` varchar(50) NOT NULL,
+  `moviename` varchar(200) NOT NULL,
+  `movietype` varchar(10) NOT NULL,
+  `moviegubun` varchar(20) NOT NULL,
+  `playdt` varchar(8) NOT NULL,
+  `starttime` varchar(5) NOT NULL,
+  `endtime` varchar(5) NOT NULL,
+  `bookingseatcount` int(11) NOT NULL,
+  `totalseatcount` int(11) NOT NULL,
+  PRIMARY KEY  (`cinemaid`,`roomid`,`moviecode`)
+) ENGINE=MyISAM DEFAULT CHARSET=euckr;
+
+-- --------------------------------------------------------
+
+--
 -- 테이블 구조 `lotto`
 --
 
@@ -185,6 +333,101 @@ CREATE TABLE `lotto` (
 -- --------------------------------------------------------
 
 --
+-- 테이블 구조 `mega_cinemas`
+--
+
+CREATE TABLE `mega_cinemas` (
+  `cinemacode` varchar(4) NOT NULL,
+  `regioncode` varchar(2) NOT NULL,
+  `cinemaname` varchar(100) NOT NULL,
+  `kofcinemacode` varchar(6) NOT NULL,
+  PRIMARY KEY  (`cinemacode`)
+) ENGINE=MyISAM DEFAULT CHARSET=euckr;
+
+-- --------------------------------------------------------
+
+--
+-- 테이블 구조 `mega_movies`
+--
+
+CREATE TABLE `mega_movies` (
+  `moviecode` varchar(6) NOT NULL,
+  `releasedate` varchar(8) NOT NULL,
+  `moviegbn` varchar(20) NOT NULL,
+  `moviename` varchar(100) NOT NULL,
+  PRIMARY KEY  (`moviecode`)
+) ENGINE=MyISAM DEFAULT CHARSET=euckr;
+
+-- --------------------------------------------------------
+
+--
+-- 테이블 구조 `mega_regions`
+--
+
+CREATE TABLE `mega_regions` (
+  `regioncode` varchar(2) NOT NULL,
+  `regionname` varchar(30) NOT NULL,
+  PRIMARY KEY  (`regioncode`)
+) ENGINE=MyISAM DEFAULT CHARSET=euckr;
+
+-- --------------------------------------------------------
+
+--
+-- 테이블 구조 `mega_tickecting1`
+--
+
+CREATE TABLE `mega_tickecting1` (
+  `playdate` varchar(8) NOT NULL,
+  `cinemacode` varchar(4) NOT NULL,
+  `moviecode` varchar(6) NOT NULL,
+  `cinemaname` varchar(100) NOT NULL,
+  `moviename` varchar(100) NOT NULL,
+  PRIMARY KEY  (`playdate`,`cinemacode`,`moviecode`)
+) ENGINE=MyISAM DEFAULT CHARSET=euckr;
+
+-- --------------------------------------------------------
+
+--
+-- 테이블 구조 `mega_tickecting2`
+--
+
+CREATE TABLE `mega_tickecting2` (
+  `playdate` varchar(8) NOT NULL,
+  `cinemacode` varchar(4) NOT NULL,
+  `moviecode` varchar(6) NOT NULL,
+  `roomno` int(11) NOT NULL,
+  `cinemaname` varchar(100) NOT NULL,
+  `moviename` varchar(100) NOT NULL,
+  `cinemaroom` varchar(20) NOT NULL,
+  `moviegubun` varchar(50) NOT NULL,
+  PRIMARY KEY  (`playdate`,`cinemacode`,`moviecode`,`roomno`)
+) ENGINE=MyISAM DEFAULT CHARSET=euckr;
+
+-- --------------------------------------------------------
+
+--
+-- 테이블 구조 `mega_tickecting3`
+--
+
+CREATE TABLE `mega_tickecting3` (
+  `playdate` varchar(8) NOT NULL,
+  `cinemacode` varchar(4) NOT NULL,
+  `moviecode` varchar(6) NOT NULL,
+  `roomno` int(11) NOT NULL,
+  `timeno` int(11) NOT NULL,
+  `cinemaname` varchar(100) NOT NULL,
+  `moviename` varchar(100) NOT NULL,
+  `moviegubun` varchar(20) NOT NULL,
+  `starttime` varchar(5) NOT NULL,
+  `endtime` varchar(5) NOT NULL,
+  `timegbn` varchar(20) NOT NULL,
+  `seat` varchar(10) NOT NULL,
+  PRIMARY KEY  (`playdate`,`cinemacode`,`moviecode`,`roomno`,`timeno`)
+) ENGINE=MyISAM DEFAULT CHARSET=euckr;
+
+-- --------------------------------------------------------
+
+--
 -- 테이블 구조 `wrk_job`
 --
 
@@ -192,30 +435,4 @@ CREATE TABLE `wrk_job` (
   `JobCode` varchar(8) NOT NULL,
   `Status` varchar(100) NOT NULL,
   `Percent` varchar(10) NOT NULL
-) ENGINE=MyISAM DEFAULT CHARSET=euckr;
-
-
-
-
-CREATE TABLE `cgv_movies` (
-  `moviecode` varchar(5) NOT NULL,
-  `moviename` varchar(100) NOT NULL,
-  `releasedate` varchar(8) NOT NULL,
-  PRIMARY KEY  (`moviecode`)
-) ENGINE=MyISAM DEFAULT CHARSET=euckr;
-
-CREATE TABLE `cgv_theater` (
-    `theatercode` varchar(4) NOT NULL,
-    `regioncode` varchar(2) NOT NULL,
-    `regionname` varchar(10) NOT NULL,
-    `theatername` varchar(100) NOT NULL,
-  PRIMARY KEY  (`theatercode`)
-) ENGINE=MyISAM DEFAULT CHARSET=euckr;
-
-
-
-CREATE TABLE `cgv_regions` (
-    `regioncode` varchar(2) NOT NULL,
-    `regionname` varchar(10) NOT NULL,
-  PRIMARY KEY  (`regioncode`)
 ) ENGINE=MyISAM DEFAULT CHARSET=euckr;
