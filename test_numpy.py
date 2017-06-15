e4a94341b28c9012723430f07aab428193ef17ca
이홍석 <lhs0806@nate.com>
2017-06-15 23:20:18 +0900

    계산과정 등록

--- test_numpy.py
+++ test_numpy.py
@@ -1,10 +1,31 @@
-
+# numpy예제
 import numpy as np
 ## import numpy.linalg as lin
 
 x = np.array([[1,2,3],[4,5,6]])
 y = np.array([[1,0,-1],[1,1,0]])
 
-t = np.dot(x,y.T)
+t = np.dot(x,y.T) # 행렬의 곱셈,,,,
 
+# x의 행렬
+print('x의 행렬')
+print(x)
+
+# y의 역행렬
+print('y의 역행렬')
+print(y.T)
+
+# 결과
+print('결과')
 print(t)
+
+# x의 행렬
+# [[1 2 3]
+#  [4 5 6]]
+# y의 역행렬
+# [[ 1  1]
+#  [ 0  1]
+#  [-1  0]]
+# 결과
+# [[-2  3]
+#  [-2  9]]
