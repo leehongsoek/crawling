0642ae39d291828d4cdf56b16692e00411511644
이홍석 <lhs0806@nate.com>
2017-10-14 17:28:48 +0900

    생성

---
+++ crawling_lotte.py
@@ -0,0 +1,337 @@
+'''
+롯데 시네마에서 자료가져오기
+'''
+
+import random
+import datetime, time
+import urllib3  # pip install urllib3
+import json
+from jsonpath_rw import jsonpath, parse  # pip install jsonpath-rw      https://pypi.python.org/pypi/jsonpath-rw
+from multiprocessing import Queue # python Setup.py build # exe 파일 생성을 위해 꼭 필요
+
+
+#------------------------------------------------------------------------------------------------------------------------------------------------------
+# 공통 변수......
+#
+dicMovieData = {}  # 영화데이터 정보
+dicCinemas = {}  # 극장 코드 정보
+dicMovies = {}  # 영화 코드 정보
+dicTicketingData = {}  # 티켓팅 정보
+
+http = urllib3.PoolManager()
+
+ostype    = "Chrome"
+osversion = "Mozilla/5.0 (Windows NT 6.1; WOW64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/55.0.2883.105 Safari/537.36 Swing/4.0.0.11"
+
+#
+#
+#
+
+#------------------------------------------------------------------------------------------------------------------------------------------------------
+# 영화 / 박스 오피스 에서 영화데이터를 가지고 온다. (dicMovieData)
+#
+def func_lotte_boxoffice():
+#
+#
+    fields = {"paramList":
+                          '{"MethodName":"GetMovies",'
+                          '"channelType":"HO",'
+                          '"osType":"' + ostype + '",'
+                          '"osVersion":"' + osversion + '",'
+                          '"multiLanguageID":"KR",'
+                          '"division":1,'
+                          '"moviePlayYN":"Y",'
+                          '"orderType":"1",'
+                          '"blockSize":100,'
+                          '"pageNo":1'
+                          '}'
+                      }
+    url = 'http://www.lottecinema.co.kr/LCWS/Movie/MovieData.aspx?nocashe=' + str( random.random() )
+    r = http.request( 'POST', url, fields )
+
+    data = r.data.decode( 'utf-8' )
+    # print(data)
+
+    json_obj = json.loads( data )
+
+    jsonpath_expr = parse( 'Movies.Items[*]' )
+
+    i = 0
+    for match in jsonpath_expr.find( json_obj ):
+        representationmoviecode = str( match.value['RepresentationMovieCode'] )
+        movienamekr             = str( match.value['MovieNameKR'] ).strip()
+        moviegenrename          = str( match.value['MovieGenreName'] )
+        bookingyn               = str( match.value['BookingYN'] )
+        releasedate             = str( match.value['ReleaseDate'] )
+        releasedate             = releasedate[0:4] + releasedate[5:7] + releasedate[8:10]
+        viewgradenameus         = str( match.value['ViewGradeNameUS'] )
+
+        if movienamekr == '' or movienamekr == 'AD': continue
+
+        i = i + 1
+        # print( str( i ) + ' :: ' + representationmoviecode + ' // ' + movienamekr + ' / ' + moviegenrename  + ' / ' + bookingyn  + ' / ' + releasedate  + ' / ' + viewgradenameus)
+
+        dicMovieData[representationmoviecode] = [movienamekr, moviegenrename, bookingyn, releasedate, viewgradenameus]  # 영화데이터 정보
+
+    fields = {"paramList":
+                          '{"MethodName":"GetMovies",'
+                          '"channelType":"HO",'
+                          '"osType":"' + ostype + '",'
+                          '"osVersion":"' + osversion + '",'
+                          '"multiLanguageID":"KR",'
+                          '"division":1,'
+                          '"moviePlayYN":"N",'
+                          '"orderType":"5",'
+                          '"blockSize":100,'
+                          '"pageNo":1'
+                          '}'
+                      }
+    url = 'http://www.lottecinema.co.kr/LCWS/Movie/MovieData.aspx?nocashe=' + str( random.random() )
+    r = http.request( 'POST', url, fields )
+
+    data = r.data.decode( 'utf-8' )
+    # print(data)
+
+    json_obj = json.loads( data )
+
+    jsonpath_expr = parse( 'Movies.Items[*]' )
+
+    i = 0
+    for match in jsonpath_expr.find( json_obj ):
+        representationmoviecode = str( match.value['RepresentationMovieCode'] )
+        movienamekr             = str( match.value['MovieNameKR'] )
+        moviegenrename          = str( match.value['MovieGenreName'] )
+        bookingyn               = str( match.value['BookingYN'] )
+        releasedate             = str( match.value['ReleaseDate'] )
+        releasedate             = releasedate[0:4] + releasedate[5:7] + releasedate[8:10]
+        viewgradenameus         = str( match.value['ViewGradeNameUS'] )
+
+        if movienamekr == '' or movienamekr == 'AD': continue
+
+        i = i + 1
+        # print( str( i ) + ' :: ' + representationmoviecode + ' // ' + movienamekr + ' / ' + moviegenrename  + ' / ' + bookingyn  + ' / ' + releasedate  + ' / ' + viewgradenameus)
+
+        dicMovieData[representationmoviecode] = [movienamekr, moviegenrename, bookingyn, releasedate, viewgradenameus]  # 영화데이터 정보
+#
+# def func_lotte_boxoffice():
+#
+
+
+#------------------------------------------------------------------------------------------------------------------------------------------------------
+# 영화관 에서 극장데이터를 가지고 온다. (dicCinemas)
+#
+def func_lotte_cinema():
+
+    specialcinemas = ["0300",  ## 스페셜관(샤롯데) 지정
+                      "0941",  ## 스페셜관(수퍼플렉스 G) 지정
+                      "0940",  ## 스페셜관(수퍼플렉스) 지정
+                      "0930",  ## 스페셜관(수퍼 4D) 지정
+                      "0910",  ## 스페셜관(수퍼바이브) 지정
+                      "0960",  ## 스페셜관(씨네패밀리) 지정
+                      "0200",  ## 스페셜관(씨네커플) 지정
+                      "0950"   ## 스페셜관(씨네비즈) 지정
+                      ]
+    for specialcinema in specialcinemas:
+        # print(specialcinema)
+
+        fields = {"paramList":
+                      '{"MethodName":"SpecialCinemaList",'
+                      '"channelType":"HO",'
+                      '"osType":"' + ostype + '",'
+                      '"osVersion":"' + osversion + '",'
+                      '"DetailDivisionCode":"' + specialcinema + '",'
+                      '"Latitude":"37.5675451", "Longitude":"126.9773356"'
+                      '}'
+                  }
+        url = 'http://www.lottecinema.co.kr/LCWS/Cinema/CinemaData.aspx?nocashe=' + str( random.random() )
+        r = http.request( 'POST', url, fields )
+
+        data = r.data.decode( 'utf-8' )
+        # print(data)
+
+        json_obj = json.loads( data )
+
+        jsonpath_expr = parse( 'Cinemas.Items[*]' )
+
+        i = 0
+        for match in jsonpath_expr.find( json_obj ):
+            cinemaid     = str( match.value['CinemaID'] )
+            cinemanamekr = match.value['CinemaNameKR']
+            sortsequence = match.value['SortSequence']
+
+            i = i + 1
+            # print( str( i ) + ' :: ' + cinemaid + ' / ' + cinemanamekr)
+
+            dicCinemas[cinemaid] = ['Y', sortsequence, cinemanamekr]  # 극장(스페셜괌)정보저장
+
+    detaildivisioncodes = ["1",   ## 서울
+                           "2",   ## 경기/인천
+                           "3",   ## 충청/대전
+                           "4",   ## 전라/광주
+                           "5",   ## 경북/대구
+                           "101", ## 경남/부산/울산
+                           "6",   ## 강원
+                           "7"    ## 제주
+                           ]
+    for detaildivisioncode in detaildivisioncodes:
+
+        fields = {"paramList":
+                      '{"MethodName":"GetCinemaByArea",'
+                      '"channelType":"HO",'
+                      '"osType":"' + ostype + '",'
+                      '"osVersion":"' + osversion + '",'
+                      '"multiLanguageID":"KR",'
+                      '"divisionCode":"1","detailDivisionCode":"' + detaildivisioncode + '"'
+                      '}'
+                  }
+        url = 'http://www.lottecinema.co.kr/LCWS/Cinema/CinemaData.aspx?nocashe=' + str( random.random() )
+        r = http.request( 'POST', url, fields )
+
+        data = r.data.decode( 'utf-8' )
+
+        json_obj = json.loads( data )
+
+        jsonpath_expr = parse( 'Cinemas.Items[*]' )
+
+        i = 0
+        for match in jsonpath_expr.find( json_obj ):
+            cinemaid     = str( match.value['CinemaID'] )
+            cinemaname   = match.value['CinemaName']
+            sortsequence = match.value['SortSequence']
+
+            i = i + 1
+            # print( str( i ) + ' :: ' + cinemaid + ' / ' + cinemaname )
+
+            dicCinemas[cinemaid] = ['N', sortsequence, cinemaname]  # 극장 정보저장
+
+#
+# def func_lotte_cinema():
+#
+
+
+def func_lotte_ticketingdata():
+
+    days = []
+
+    date1 = datetime.date.today()  ## 오늘자 날짜객체
+    date2 = date1 + datetime.timedelta( days=1 )
+    date3 = date2 + datetime.timedelta( days=1 )
+
+    days.append( '{:04d}-{:02d}-{:02d}'.format( date1.year, date1.month, date1.day ) )  ## 오늘의 날짜
+    days.append( '{:04d}-{:02d}-{:02d}'.format( date2.year, date2.month, date2.day ) )  ## 오늘+1의 날짜
+    days.append( '{:04d}-{:02d}-{:02d}'.format( date3.year, date3.month, date3.day ) )  ## 오늘+2의 날짜
+
+    # 3일간 자료 가져오기
+    for today in days:
+        #
+        # 전체극장을 돌면서 각각의 정보를 가져온다.
+        #
+        for dicCinema in dicCinemas:
+            # print(dicCinema +'//' +str(dicCinemas[dicCinema]))
+            # if dicCinema != '6005':
+            #     continue
+
+            fields = {"paramList":
+                          '{"MethodName":"GetPlaySequence",'
+                          '"channelType":"HO",'
+                          '"osType":"' + ostype + '",'
+                          '"osVersion":"' + osversion + '",'
+                          '"playDate":"' + today + '",'
+                          '"cinemaID":"1|1|' + dicCinema + '",'
+                          '"representationMovieCode":""'
+                          '}'
+                      }
+            url = 'http://www.lottecinema.co.kr/LCWS/Ticketing/TicketingData.aspx?nocashe=' + str( random.random() )
+
+            r = http.request( 'POST', url, fields )
+            # status = r.status
+            data = r.data.decode( 'utf-8' )
+            # print( data )
+
+            json_obj = json.loads( data )
+            # print(json_obj['PlaySeqsHeader'])
+
+            jsonpath_expr1 = parse( 'PlaySeqsHeader.Items[*]' )
+            jsonpath_expr2 = parse( 'PlaySeqs.Items[*]' )
+
+            i = 0
+            for match1 in jsonpath_expr1.find( json_obj ):
+                moviecode  = match1.value['MovieCode']
+                gubun      = match1.value['TranslationDivisionNameKR']  # 더빙/자막
+                moviename  = match1.value['MovieNameKR']                # 영화명
+                filmnamekr = match1.value['FilmNameKR']                 # 필름종류
+
+                dicMovies[moviecode] = [moviename, filmnamekr, gubun]  # 영화정보를 저장한다. 영화명 + 필름종류 + 더빙/자막
+                i = i + 1
+                # print(str(i)+' :: '+match1.value['CinemaNameKR']+' / '+moviecode+' / '+' / '+ moviename +' / '+gubun)
+
+                # for match1_1 in jsonpath_expr1.find( json_obj ):
+                # i = i + 1
+                # print( i)
+
+            i = 0
+            for match2 in jsonpath_expr2.find( json_obj ):
+                cinemanamekr          = match2.value['CinemaNameKR']           # 극장명
+                sequencenogroupnamekr = match2.value['SequenceNoGroupNameKR']  # 상영관그룹명
+                screenid              = match2.value['ScreenID']               # 상영관 코드
+                screennamekr          = match2.value['ScreenNameKR']           # 상영관명
+                moviecode             = match2.value['MovieCode']              # 영화코드 [영화명 + 필름종류 + 더빙/자막]
+                bookingseatcount      = match2.value['BookingSeatCount']       # 예약좌석수
+                totalseatcount        = match2.value['TotalSeatCount']         # 총좌석수
+                playdt                = match2.value['PlayDt']                 # 상영일자
+                playdt                = playdt[0:4] + playdt[5:7] + playdt[8:10]
+                starttime             = match2.value['StartTime']              # 상영시간(시작)
+                endtime               = match2.value['EndTime']                # 상영시간(끝)
+                # print(dicMovies[moviecode][1]+':'+dicMovies[moviecode][0])
+
+                ticketcode = str(screenid) + str(moviecode)
+                dicTicketingData[ticketcode] = [cinemanamekr, sequencenogroupnamekr, screennamekr, dicMovies[moviecode][0], dicMovies[moviecode][1], dicMovies[moviecode][2], playdt, starttime, endtime, bookingseatcount, totalseatcount]
+
+                i = i + 1
+                # print( str( i ) + ' :: '
+                #        + playdt + ' / '  # 상영일자
+                #        + cinemanamekr + ' / '  # 극장명
+                #        + sequencenogroupnamekr + ' / '  # 상영관그룹명
+                #        + str( screenid ) + ' (' + screennamekr + ') / '  # 상영관코드(명)
+                #        + starttime + '~' + endtime + ' / '  # 상영시간
+                #        + str( moviecode ) + ' (' + dicMovies[moviecode][0] + '[' + dicMovies[moviecode][1] + '/' +
+                #        dicMovies[moviecode][2] + ']) / '  ##  영화명 + 필름종류 + 더빙/자막
+                #        + str( bookingseatcount ) + '/' + str( totalseatcount )  # 예약좌석수 / 총좌석수
+                #        )
+                # print(match.value)
+
+                # print('----------------------------------------')
+                # for movkey in dicMovies.keys():
+                #     print(movkey +':'+ str(dicMovies[movkey]))
+                # print('----------------------------------------')
+                #
+
+
+def func_lotte_upload():
+
+    fields = { "moviedata": str( dicMovieData )
+             , "cinemas": str( dicCinemas )
+             , "ticketingdata": str( dicTicketingData )
+             }
+    url = 'http://www.mtns7.co.kr/totalscore/lotte_upload.php'
+
+    r = http.request( 'POST', url, fields )
+
+    data = r.data.decode( 'utf-8' )
+    print( data )
+
+if  __name__ == '__main__':
+
+    func_lotte_boxoffice()
+
+    func_lotte_cinema()
+    # for dicCinema in dicCinemas:
+    #     print( dicCinema + ' // ' + str( dicCinemas[dicCinema] ) )
+
+    func_lotte_ticketingdata()
+    # for dicMovie in dicMovies:
+    #     print( dicMovie + ' // ' + str( dicMovies[dicMovie] ) )
+    # for dicTicketingDatum in dicTicketingData:
+    #     print( dicTicketingDatum + ' // ' + str( dicTicketingData[dicTicketingDatum] ) )
+
+    func_lotte_upload()
