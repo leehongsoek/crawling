bbd72f5921c2c8a5664b7f59266a91bc58a548e9
이홍석 <lhs0806@nate.com>
2017-10-15 13:07:18 +0900

    import 수정

--- crawling_all.py
+++ crawling_all.py
@@ -7,40 +7,38 @@ http://www.lottecinema.co.kr/
 http://www.megabox.co.kr/
 
 '''
-import crawling_cgv
-import crawling_mega
-import crawling_lotte
+import crawling_cgv as cgv
+import crawling_mega as mega
+import crawling_lotte as lotte
 
 if  __name__ == '__main__':
 
     # CGV
-    crawl_cgv_moviefinder( True )  # 영화/무비파인더(http://www.cgv.co.kr/movies/finder.aspx) 에서 영화데이터를 가지고 온다. (dicMovies) - 화면 서비스가 정지 될 수 있어서.. 그 경우 위의 함수를 호출한다.
+    cgv.crawl_cgv_moviefinder( True )  # 영화/무비파인더(http://www.cgv.co.kr/movies/finder.aspx) 에서 영화데이터를 가지고 온다. (dicMovies) - 화면 서비스가 정지 될 수 있어서.. 그 경우 위의 함수를 호출한다.
 
-    crawl_cgv_theaters( True )  # 예매/상영시간표(http://www.cgv.co.kr/reserve/show-times/) 극장정보를 가지고 온다. (dicTheaters)
+    cgv.crawl_cgv_theaters( True )  # 예매/상영시간표(http://www.cgv.co.kr/reserve/show-times/) 극장정보를 가지고 온다. (dicTheaters)
 
-    crawl_cgv_showtimes( True )  # 예매/상영시간표(http://www.cgv.co.kr/reserve/show-times/)의 프래임에서 상영정보를 가지고 온다. (dicTicketMovies)
+    cgv.crawl_cgv_showtimes( True )  # 예매/상영시간표(http://www.cgv.co.kr/reserve/show-times/)의 프래임에서 상영정보를 가지고 온다. (dicTicketMovies)
 
-    crawl_cgv_upload()
+    cgv.crawl_cgv_upload()
 
     # LOTTE
 
-    crawl_lotte_boxoffice( False )  # 영화 / 박스 오피스(http://www.lottecinema.co.kr/LCHS/Contents/Movie/Movie-List.aspx) 에서 영화데이터를 가지고 온다. (dicMovieData)
+    lotte.crawl_lotte_boxoffice( True )  # 영화 / 박스 오피스(http://www.lottecinema.co.kr/LCHS/Contents/Movie/Movie-List.aspx) 에서 영화데이터를 가지고 온다. (dicMovieData)
 
-    crawl_lotte_cinema( False )  # 영화관 (http://www.lottecinema.co.kr/LCHS/Contents/Cinema) 에서 극장데이터를 가지고 온다. (dicCinemas)
+    lotte.crawl_lotte_cinema( True )  # 영화관 (http://www.lottecinema.co.kr/LCHS/Contents/Cinema) 에서 극장데이터를 가지고 온다. (dicCinemas)
 
-    crawl_lotte_ticketingdata( True )  # 영화관 (http://www.lottecinema.co.kr/LCWS/Ticketing/TicketingData.aspx) 에서 극장데이터를 가지고 온다. (dicTicketingData)
+    lotte.crawl_lotte_ticketingdata( True )  # 영화관 (http://www.lottecinema.co.kr/LCWS/Ticketing/TicketingData.aspx) 에서 극장데이터를 가지고 온다. (dicTicketingData)
 
     # MEGA BOX
 
-    crawl_mega_movie(True) # 영화(http://www.megabox.co.kr/?menuId=movie) 에서 영화데이터를 가지고 온다. (dicMovies)
+    mega.crawl_mega_movie(True) # 영화(http://www.megabox.co.kr/?menuId=movie) 에서 영화데이터를 가지고 온다. (dicMovies)
 
-    crawl_mega_cinema(True) # 영화관(http://www.megabox.co.kr/?menuId=theater)에서 지역데이터,영화관데이터를 가지고 온다. (dicRegions,dicCinemas)
+    mega.crawl_mega_cinema(True) # 영화관(http://www.megabox.co.kr/?menuId=theater)에서 지역데이터,영화관데이터를 가지고 온다. (dicRegions,dicCinemas)
 
-    crawl_mega_schedule(True) # 영화관(http://www.megabox.co.kr/?menuId=theater)에서 영화관에 스케줄데이터를 가지고 온다. (dicRegions,dicCinemas)
+    mega.crawl_mega_schedule(True) # 영화관(http://www.megabox.co.kr/?menuId=theater)에서 영화관에 스케줄데이터를 가지고 온다. (dicRegions,dicCinemas)
 
-    crawl_mega_upload()
+    mega.crawl_mega_upload()
 
 # if  __name__ == '__main__':
-
-
 
