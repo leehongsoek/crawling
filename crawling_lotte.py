d3f57e125a2ec5eec8aaa75fcb971fa9b390c8ad
이홍석 <lhs0806@nate.com>
2017-10-15 20:21:47 +0900

    1 -> 3 일

--- crawling_lotte.py
+++ crawling_lotte.py
@@ -253,8 +253,8 @@ def crawl_lotte_ticketingdata(isPrnConsole):
     date3 = date2 + datetime.timedelta( days=1 )
 
     days.append( '{:04d}-{:02d}-{:02d}'.format( date1.year, date1.month, date1.day ) )  ## 오늘의 날짜
-    #days.append( '{:04d}-{:02d}-{:02d}'.format( date2.year, date2.month, date2.day ) )  ## 오늘+1의 날짜
-    #days.append( '{:04d}-{:02d}-{:02d}'.format( date3.year, date3.month, date3.day ) )  ## 오늘+2의 날짜
+    days.append( '{:04d}-{:02d}-{:02d}'.format( date2.year, date2.month, date2.day ) )  ## 오늘+1의 날짜
+    days.append( '{:04d}-{:02d}-{:02d}'.format( date3.year, date3.month, date3.day ) )  ## 오늘+2의 날짜
 
     # 3일간 자료 가져오기
     for today in days:
