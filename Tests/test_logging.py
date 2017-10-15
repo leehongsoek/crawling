ba1d64d6e19e3d49241b60ab9f820b38559bffce
이홍석 <lhs0806@nate.com>
2017-10-15 20:22:35 +0900

    로깅테스트 시작

--- Tests/test_logging.py
+++
@@ -1,9 +0,0 @@
-
-import logging # 파일로 남기기 위해 filename='./log/test.log' parameter로 추가한다.
-
-logging.basicConfig(filename='./log/test.log', level=logging.DEBUG)
-logging.debug("debug")
-logging.info("info")
-logging.warning("warning")
-logging.error("error")
-logging.critical("critical")
